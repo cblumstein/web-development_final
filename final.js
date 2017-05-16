45dde248142e5612ebcb3b1496d2418271097d35
Cara Blumstein <cblumstein@wesleyan.edu>
2017-05-15 22:53:18 -0400

    Add files via upload

---
+++ final.js
@@ -0,0 +1,12 @@
+var navBar = document.getElementById("nav");
+var content = document.getElementById("projects")
+
+var toggleBtns = Array.from(document.getElementsByClassName("nav-toggle"));
+
+toggleBtns.forEach(function(btn) {
+	btn.addEventListener("click", function(e) {
+		e.preventDefault();
+		navBar.classList.toggle("active");
+		content.classList.toggle("active");
+	});
+});
